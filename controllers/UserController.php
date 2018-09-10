ea06be3236937455ba5f2ea96911fa2f28212e3f
IT FAN <1724940950@qq.com>
2018-09-10 21:57:21 +0800

    ...

---
+++ controllers/UserController.php
@@ -0,0 +1,14 @@
+<?php
+namespace controllers;
+use models\User;
+class UserController{
+    public function hello(){
+        // 取数据
+        $user = new User;
+        $name = $user->getName();
+        // 加载视图
+        view('users.hello',[
+            'name'=>$name,
+        ]);
+    }
+}
--- public/index.php
+++ public/index.php
@@ -1,1 +1,15 @@
 <?php
+// 当前文件的绝对路径
+define('ROOT',dirname(__FILE__).'/../');
+
+function autoload($class){
+   $path = str_replace('\\','/',$class);
+    require(ROOT.$path.'.php');
+}
+spl_autoload_register('autoload');
+
+$userController = new controllers\UserController;
+$userController->hello();
+function view ($viewFileName,$data = []){
+    
+}
